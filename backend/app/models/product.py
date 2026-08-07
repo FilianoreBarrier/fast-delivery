f5167a8c4b6f8a638911278ffa0377c0a190166c
FilianoreBarrier <maric.zubenko@gmail.com>
2026-08-07 19:30:28 +0500

    fixes

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timezone
+from typing import TYPE_CHECKING
 from sqlalchemy.orm import relationship, mapped_column, Mapped
+if TYPE_CHECKING:
+    from .category import Category
 from sqlalchemy import (Text, ForeignKey, DateTime)
 from sqlalchemy.sql import func
 from app.core.database import Base
@@ -11,13 +14,12 @@ class Product(Base):
     name: Mapped[str] = mapped_column(index=True)
     description: Mapped[str|None] = mapped_column(Text)
     price: Mapped[float] = mapped_column()
-    image_url:Mapped[str] = mapped_column()
+    image_url:Mapped[str|None] = mapped_column()
     created_at:Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
 
     category_id: Mapped[int] = mapped_column(ForeignKey("category.id",ondelete ="CASCADE"))
 
-
-    category = relationship("Category",back_populates="products")
+    category: Mapped[Category] = relationship(back_populates='categories')
 
     def __repr__(self):
         return f'<Product(id={self.id},name = "{self.name}",price ={self.price})>'
